68bc449fbe8466fbdce6b7531d2d8e34bb462c23
dashingshakeel <shkeelrauf@gmail.com>
2016-08-09 16:46:23 +0500

    set design and organition of app

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -43,8 +43,7 @@ logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
 //     end
 //
 // Finally, pass the token on connect as below. Or remove it
-// from connect if you don't care about authentication.
-let socket=new Socket("/socket")
+
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
